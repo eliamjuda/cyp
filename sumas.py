febf1f50fb60091a99caafa56c05becccc48cc06
eliamjuda <eliamjuda@gmail.com>
2021-10-12 17:58:43 -0500

    First commit

---
+++ sumas.py
@@ -0,0 +1,14 @@
+a = 0
+b = 0
+c = 0
+
+# print("Dame un valor numérico")
+
+a = int(input("Dame un valor numérico"))
+b = int(input("Dame otro valor numérico"))
+
+c = a + b;
+print( c )
+
+print("es mayor a 10") if c > 10 else print("es menor a 10")
+
