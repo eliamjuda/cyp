7864f4888738649e100a42b717ed8931fc127a28
eliamjuda <eliamjuda@gmail.com>
2021-10-21 17:51:14 -0500

    Tarea 4

---
+++ tarea4.py
@@ -0,0 +1,9 @@
+# EEEE LL    II     AA     MMMM MMMM 
+# E    LL    II    A  A    MM MM  MM 
+# EEEE LL    II   AAAAAA   MM     MM 
+# E    LL    II  A      A  MM     MM 
+# EEEE LLLLL II A        A MM     MM 
+
+print("Tarea - Nombre de pila: \n")
+print("EEEE LL    II     AA     MMMM MMMM\nE    LL    II    A  A    MM MM  MM\nEEEE LL    II   AAAAAA   MM     MM\nE    LL    II  A      A  MM     MM\nEEEE LLLLL II A        A MM     MM")
+print("\n")
