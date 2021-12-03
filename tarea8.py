a6a8e52f8d0bcfdd602fd97ed5401b65d91e86cd
eliamjuda <eliamjuda@gmail.com>
2021-12-02 18:16:45 -0600

    Tarea 8

--- tarea8.py
+++ tarea8.py
@@ -3,6 +3,32 @@
 # EEEE LL    II   AAAAAA   MM     MM 
 # E    LL    II  A      A  MM     MM 
 # EEEE LLLLL II A        A MM     MM 
+ctrE = input("¿Qué caracter quieres para la E? ")
+ctrL = input("¿Qué caracter quieres para la L? ")
+ctrI = input("¿Qué caracter quieres para la I? ")
+ctrA = input("¿Qué caracter quieres para la A? ")
+ctrM = input("¿Qué caracter quieres para la M? ")
 
-def letraE( caracter ):
-    print(f"{caracter}{caracter}{caracter}")
+
+
+def letraE( ctr ):
+    return print(f"\n{ctr}{ctr}{ctr} \n{ctr} \n{ctr}{ctr}{ctr} \n{ctr} \n{ctr}{ctr}{ctr}\n")
+
+def letraL( ctr ):
+    return print(f"{ctr}{ctr}\n{ctr}{ctr}\n{ctr}{ctr}\n{ctr}{ctr}\n{ctr}{ctr}{ctr}{ctr}{ctr}\n")
+
+def letraI( ctr ):
+    return print(f"{ctr}{ctr}\n{ctr}{ctr}\n{ctr}{ctr}\n{ctr}{ctr}\n{ctr}{ctr}\n")
+
+def letraA( ctr ):
+    return print(f"    {ctr}{ctr}\n   {ctr}  {ctr}\n  {ctr}{ctr}{ctr}{ctr}{ctr}{ctr}\n {ctr}      {ctr}\n{ctr}        {ctr}\n")
+
+def letraM( ctr ):
+    return print(f"{ctr}{ctr}{ctr}{ctr} {ctr}{ctr}{ctr}{ctr}\n{ctr}{ctr} {ctr}{ctr}  {ctr}{ctr}\n{ctr}{ctr}     {ctr}{ctr}\n{ctr}{ctr}     {ctr}{ctr}\n{ctr}{ctr}     {ctr}{ctr}\n")
+
+
+letraE(ctrE)
+letraL(ctrL)
+letraI(ctrI)
+letraA(ctrA)
+letraM(ctrM)
